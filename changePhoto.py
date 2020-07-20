21468a69fd0fecab31f399f017ea577128684666
9831023 <9831023@ceit.local>
2020-07-20 18:07:43 +0430

    photo converter file

--- changePhoto.py
+++ changePhoto.py
@@ -1,4 +1,15 @@
 import cv2
 import numpy as np
+import binLibrary
 
-image = cv2.imread()
+def changePhoto(stringFileName, stringCode):
+    image = cv2.imread(stringFileName)
+    heigh, width, color = image.shape
+    codeList = binLibrary.stringToBin(stringCode)
+    index = 0 
+    for i in range(heigh):
+        for j in range(width):
+            for c in range(color):
+                
+                
+
