9c24bf7cc9c9732ddd5ea584200eea2f59eff41b
9831023 <9831023@ceit.local>
2020-07-29 02:58:50 +0430

    exceptions handled

--- changePhoto.py
+++ changePhoto.py
@@ -6,36 +6,44 @@ import re
 
 def changePhoto(stringFileName, stringCode, nameCodedFile):
     image = cv2.imread(stringFileName)
-    # if(image==None):
+    # if(image==None): 
     #     print("the file does not exist")
     #     sys.exit(0)
-    heigh, width, color = image.shape
-    if(heigh*width*3<len(stringCode)*8):
-        print("the image is too small")
-        return
-    codeList = binLibrary.stringToBin(stringCode)
-    index = 0 
-    # in this type of iterating the index 0 is blue 
-    # 1 is green and 2 is red thats why the last for is 
-    # reverse 
-    for i in range(heigh):
-        for j in range(width):
-            c = 2
-            while(c>=0):
-                # print(image[i,j,c])
-                binaryColor = binLibrary.numtoBin(image[i,j,c])
-                # binaryColor is a list with 8 elements(the color in binary mode)
-                if(binaryColor[7]!=codeList[index]):
-                    # print(i,j,c,"  ", binaryColor[7],"!=" , codeList[index])
-                    binaryColor[7]=codeList[index]
-                    image[i,j,c]= binLibrary.binToNum(binaryColor)
-                index+=1
-                c-=1
-                if(index==len(codeList)):
-                    cv2.imwrite(nameCodedFile, image)
-                    return
-# changePhoto("AUT_HotChocolate.bmp", "helloThereBaby", "codedFile.jpg")
-changePhoto("spring.jpg", "helloThereBaby im hereeee but you cant see me", "codedFile.jpg")
+    try:
+        heigh, width, color = image.shape
+        if(heigh*width*3<len(stringCode)*8):
+            print("the image is too small")
+            return
+        codeList = binLibrary.stringToBin(stringCode)
+        index = 0 
+        # in this type of iterating the index 0 is blue 
+        # 1 is green and 2 is red thats why the last for is 
+        # reverse 
+        for i in range(heigh):
+            for j in range(width):
+                c = 2
+                while(c>=0):
+                    binaryColor = binLibrary.numtoBin(image[i,j,c])
+                    # binaryColor is a list with 8 elements(the color in binary mode)
+                    if(binaryColor[7]!=codeList[index]):
+                        # print(i,j,c,"  ", binaryColor[7],"!=" , codeList[index])
+                        binaryColor[7]=codeList[index]
+                        image[i,j,c]= binLibrary.binToNum(binaryColor)
+                    index+=1
+                    c-=1
+                    if(index==len(codeList)):
+                        cv2.imwrite(nameCodedFile, image)
+                        return
+    except:
+        print('\033[91m',"the files selected are either not images or do not exist", '\033[0m')  
+        return 
+# changePhoto("AUT_HotChocolate.bmp", "helloThereBaby", "codedFile.bmp")
+    #works with bmp -> bmp
+# changePhoto("spring.jpg", "helloThereBaby im hereeee but you cant see me", "codedFile.jpg")
+    #does not work with jpg -> jpg
+# changePhoto("AUT_HotChocolate.png", "hello There Baby", "codedFile.png")
+    #works with png -> png
+
                 
 def changePhotoFile(stringFileName, fileCodeHolder, nameCodedFile):
     fileCode = open(fileCodeHolder, "rb")
--- decoder.py
+++ decoder.py
@@ -5,38 +5,44 @@ import binLibrary
 def decoder(nameSecundary, namePrimmary):
     image1 = cv2.imread(namePrimmary)
     image2 = cv2.imread(nameSecundary)
-    heigh1, width1, color1 = image1.shape
-    heigh2, width2, color2 = image2.shape
+    try:
+        heigh1, width1, color1 = image1.shape
+        heigh2, width2, color2 = image2.shape
 
+        if(width1 != width2 or heigh1 != heigh2 or color1!=color2 ):
+            print("the images are not equal")
+            return
 
-    if(width1 != width2 or heigh1 != heigh2 or color1!=color2 ):
-        print("the images are not equal")
-        return
+        fo = open("encryptedMessage.txt", "w")
+        codedList = []
+        zeroRepeted=0
+        for h in range(heigh1):
+            for w in range(width1):
+                c=2
+                while(c>=0):
+                    # print(binLibrary.numtoBin( image2[h,w,c]) ,binLibrary.numtoBin( image1[h,w,c]))
+                    diff = image2[h, w, c] - image1[h, w, c]
+                    # print(diff)
+                    # print("the first image ", image1[h,w,c], "   the second image ", image2[h,w,c], "  the difference= " ,diff)
+                    if(diff == 0):
+                        zeroRepeted+=1
+                        codedList.append((binLibrary.numtoBin(image2[h,w,c]))[7])  
+                        if(zeroRepeted==14):
+                            codedList[len(codedList)-8:len(codedList)] = []
+                            fo.close()
+                            return binLibrary.binToStr(codedList)                  
+                    elif(diff==-1 or diff==255):
+                        codedList.append(0)
+                        zeroRepeted=0
+                    elif(diff==1):
+                        codedList.append(1)
+                        zeroRepeted=0
+                    c-=1
 
-    fo = open("encryptedMessage.txt", "w")
-    codedList = []
-    zeroRepeted=0
-    for h in range(heigh1):
-        for w in range(width1):
-            c=2
-            while(c>=0):
-                # print(binLibrary.numtoBin( image2[h,w,c]) ,binLibrary.numtoBin( image1[h,w,c]))
-                diff = image2[h, w, c] - image1[h, w, c]
-                # print("the first image ", image1[h,w,c], "   the second image ", image2[h,w,c], "  the difference= " ,diff)
-                if(diff == 0):
-                    zeroRepeted+=1
-                    codedList.append((binLibrary.numtoBin(image2[h,w,c]))[7])  
-                    if(zeroRepeted==14):
-                        codedList[len(codedList)-8:len(codedList)] = []
-                        fo.close()
-                        return binLibrary.binToStr(codedList)                  
-                elif(diff==-1 or diff==255):
-                    codedList.append(0)
-                    zeroRepeted=0
-                elif(diff==1):
-                    codedList.append(1)
-                    zeroRepeted=0
-                c-=1
-    
+    except:
+        print('\033[91m',"the files selected are either not images or do not exist", '\033[0m')  
+        return ""  
 
-print(decoder("codedFile.jpg", "spring.jpg"))
+# print(decoder("codedFile.jpg", "spring.jpg"))
+# print(decoder("codedFile.bmp", "AUT_HotChocolate.bmp"))
+print(decoder("codedFile.png", "AUT_HotChocolate.png"))
